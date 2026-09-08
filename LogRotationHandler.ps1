d19d2bab5d9a730337e60b9e7306d2b78e3262e9
Brian-W-Enghouse <brian.willicott@enghouse.com>
2026-09-08 22:22:45 +1200

    Fix for log location

--- LogRotationHandler.ps1
+++ LogRotationHandler.ps1
@@ -1,6 +1,6 @@
 <#
 .DESCRIPTION
-	Brians Log Rotation Handler Script - Version 0.9.1
+	Brians Log Rotation Handler Script - Version 0.9.2
     Change: Source folder paths are now dynamically resolved from the drive root of the TEMP environment variable unless -SourceFolders is explicitly supplied.
 	Process Overview
 	1	-	Stops syslog service/processes.
@@ -48,7 +48,7 @@ param(
     [string[]]$SourceFolderNames = @(
         "syslogd",
         "ReportedProblems",
-        "DesignerLogs",
+        "CosmoDesigner",
         "CrashDumps",
         "IIS"
     ),
