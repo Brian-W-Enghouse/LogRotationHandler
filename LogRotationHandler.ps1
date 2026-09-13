659f68aaaaf0c490528d2c53b5260f794cbe0c23
Brian-W-Enghouse <brian.willicott@enghouse.com>
2026-09-14 11:53:36 +1200

    Version Updated

--- LogRotationHandler.ps1
+++ LogRotationHandler.ps1
@@ -1,6 +1,6 @@
 <#
 .DESCRIPTION
-	Brians Log Rotation Handler Script - Version 1.0.2
+	Brians Log Rotation Handler Script - Version 1.1.0
     Change: Source folder paths are now dynamically resolved from the drive root of the TEMP environment variable unless -SourceFolders is explicitly supplied.
 	Process Overview
 	1	-	Stops syslog service/processes.
